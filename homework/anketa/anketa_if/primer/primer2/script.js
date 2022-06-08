7a296c13143a4285d48eed101b0191d78139861b
Surmach_Serhey <saturn6862909@gmail.com>
2022-06-08 14:06:39 +0300

    fffd9999999

--- homework/anketa/anketa_if/primer/primer2/script.js
+++ homework/anketa/anketa_if/primer/primer2/script.js
@@ -13,28 +13,11 @@ while (true) {
 var ageDay = (age * 365);
 var ageEar = (age + 5);
 
-var gender = confirm('Ваш пол мужской):'); {
-	if (gender = ('мужской'));
-	else
-		gender = ('женский');
-}
+var gender = confirm('Ваш пол мужской:');
+gender == true ? gender = ('мужской') : gender = ('женский');
 
 var ageM
 ageM = age
-if (age <= 62 && gender == 'мужской')
-	ageM = ('нет');
-else if (age > 62 && gender == 'мужской')
-	ageM = ('да');
-else if (age <= 58) {
-	if (gender == 'женский' && age < 58)
-		ageM = ('нет');
-	else
-		ageM = ('нет');
-}
-else {
-	if (gender == 'женский' && age > 58)
-		ageM = ('да');
-	else
-		ageM = ('да');
-}
+age <= 62 && gender == 'мужской' ? ageM = ('нет') : age > 62 && gender == 'мужской' ? ageM = ('да') : age <= 58 ? gender == 'женский' && age < 58 ? ageM = ('нет') : ageM = ('нет') : gender == 'женский' && age > 58 ? ageM = ('да') : ageM = ('да');
+
 alert('Ваше ФИО: ' + firstName + ' ' + myName + ' ' + secondName + '\nВаш возраст в годах: ' + age + ' лет' + '\n' + 'Ваш возраст в днях: ' + ageDay + ' дней' + '\n' + 'Ваш возраст через 5 лет: ' + ageEar + ' лет' + '\n' + 'Ваш пол: ' + gender + ' ' + '\n' + 'Вы на пенсии: ' + ageM + ' ')
