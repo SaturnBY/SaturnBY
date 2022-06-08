362d705fd78c4c9e219b47f31fcc5bec65673b90
Surmach_Serhey <saturn6862909@gmail.com>
2022-06-08 13:28:08 +0300

    fffd777

--- homework/anketa/anketa_if/primer/primer2/script.js
+++ homework/anketa/anketa_if/primer/primer2/script.js
@@ -1,21 +1,24 @@
-
+var firstName = prompt('Введите фамилию:')
+var myName = prompt('Введите имя:')
+var secondName = prompt('Введите отчество:')
 var ageStr = prompt('Введите ваш возраст:');
+
 while (true) {
 	var age = parseInt(ageStr);
 	if (!isNaN(age))
 		break; // введено корректное число - выходим из цикла
 	ageStr = prompt('Введите ваш возраст корректно:');
 }
+
 var ageDay = (age * 365);
 var ageEar = (age + 5);
-var gender = confirm('Ваш пол мужской:'); {
-	if (gender == true)
-		(gender = ('мужской'));
 
+var gender = confirm('Ваш пол мужской):'); {
+	if (gender = ('мужской'));
 	else
-		(gender = ('женский'));
-
+		gender = ('женский');
 }
+
 var ageM
 ageM = age
 if (age <= 62 && gender == 'мужской')
@@ -34,5 +37,4 @@ else {
 	else
 		ageM = ('да');
 }
-
-alert('Ваш возраст в годах: ' + age + ' лет' + '\n' + 'Ваш возраст в днях: ' + ageDay + ' дней' + '\n' + 'Ваш возраст через 5 лет: ' + ageEar + ' лет' + '\n' + 'Ваш пол: ' + gender + ' ' + '\n' + 'Вы на пенсии: ' + ageM + ' ')
+alert('Ваше ФИО: ' + firstName + ' ' + myName + ' ' + secondName + '\nВаш возраст в годах: ' + age + ' лет' + '\n' + 'Ваш возраст в днях: ' + ageDay + ' дней' + '\n' + 'Ваш возраст через 5 лет: ' + ageEar + ' лет' + '\n' + 'Ваш пол: ' + gender + ' ' + '\n' + 'Вы на пенсии: ' + ageM + ' ')
