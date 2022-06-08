6e3f83ce5d89990f824e89e825fad8abe52cc0c5
Surmach_Serhey <saturn6862909@gmail.com>
2022-06-08 12:16:17 +0300

    fffd

---
+++ homework/anketa/anketa_if/primer/script.js
@@ -0,0 +1,36 @@
+
+var ageStr = prompt('Введите ваш возраст:');
+while (true) {
+	var age = parseInt(ageStr);
+	if (!isNaN(age))
+		break; // введено корректное число - выходим из цикла
+	ageStr = prompt('Введите ваш возраст корректно:');
+}
+var ageDay = (age * 365);
+var ageEar = (age + 5);
+var gender = prompt('Введите пол (мужской или женский):'); {
+	if (gender == 'мужской')
+		gender = ('мужской');
+	else
+		gender = ('женский');
+}
+var ageM
+ageM = age
+if (age <= 62 && gender == 'мужской')
+	ageM = ('нет');
+else if (age > 62 && gender == 'мужской')
+	ageM = ('да');
+else if (age <= 58) {
+	if (gender == 'женский' && age < 58)
+		ageM = ('нет');
+	else
+		ageM = ('нет');
+}
+else {
+	if (gender == 'женский' && age > 58)
+		ageM = ('да');
+	else
+		ageM = ('да');
+}
+
+alert('Ваш возраст в годах: ' + age + ' лет' + '\n' + 'Ваш возраст в днях: ' + ageDay + ' дней' + '\n' + 'Ваш возраст через 5 лет: ' + ageEar + ' лет' + '\n' + 'Ваш пол: ' + gender + ' ' + '\n' + 'Вы на пенсии: ' + ageM + ' ')
